4818745947f9b0f98f9f98afa41d93966c6a59ba
superspeeder <andy@delusion.org>
2018-08-19 17:42:28 -0700

    Update basic_parts.py

--- modular/basic_parts.py
+++ modular/basic_parts.py
@@ -1,2 +1,8 @@
 import pygame
 from pygame.locals import SCRALPHA
+
+class FilledRectangle(pygame.sprite.Sprite):
+  def __init__(self, color, size, pos):
+    pygame.sprite.Sprite.__init__(self)
+    self.image = pygame.Surface(size)
+    pygame.draw.rect(self.image, color, pygame.Rect((0,0), size)
